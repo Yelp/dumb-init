b5ce46e43b94d0155f230749b9e7f149e7c4c89e
Michael Smith <msmith@mindbridge.ai>
2021-06-08 13:04:50 -0400

    setup.py: Add license classifier
    Adds a few PyPi classifiers. In particular, the License classifier should avoid `pip-licenses` reporting the license as `UNKNOWN`.

--- setup.py
+++ setup.py
@@ -134,4 +134,10 @@ setup(
         'install_cexe': install_cexe,
     },
     distclass=ExeDistribution,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Programming Language :: C',
+    ],
 )
